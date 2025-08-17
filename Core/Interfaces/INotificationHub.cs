63ed50cf7774afd7db6d8cc5c6fc51a4c453ca79
Ramy Alaa Mahana <ramymahana7@gmail.com>
2025-08-17 03:30:11 +0300

    some fixes

--- Core/Interfaces/INotificationHub.cs
+++
@@ -1,9 +0,0 @@
-using Core.DTOs.NotificationDTOs;
-
-namespace Core.Interfaces
-{
-    public interface INotificationHub
-    {
-        Task SendNotificationAsync(string userId, NotificationDTO notification);
-    }
-} 
